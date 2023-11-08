dde8f10dc94d6ecbd704a74ef01c1a96e43146e7
Crap1nyk <btech10445.21@bitmesra.ac.in>
2023-11-08 23:20:48 +0530

    commit

--- index.php
+++ index.php
@@ -7,18 +7,301 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movie Search</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css\style.css"
 </head>
 <body>
-    <h1>MoviesMania</h1>
+
     <header>
-        
+    <nav class="navbar" data-navbar>
+
+<div class="navbar-top">
+
+  <a href="./index.html" class="logo">
+    <img src="./assets/images/logo.svg" alt="Filmlane logo">
+  </a>
+
+  <button class="menu-close-btn" data-menu-close-btn>
+    <ion-icon name="close-outline"></ion-icon>
+  </button>
+
+</div>
+
+<ul class="navbar-list">
+
+  <li>
+    <a href="./index.html" class="navbar-link">Home</a>
+  </li>
+
+  <li>
+    <a href="#" class="navbar-link">Movie</a>
+  </li>
+
+  <li>
+    <a href="#" class="navbar-link">Tv Show</a>
+  </li>
+
+  <li>
+    <a href="#" class="navbar-link">Web Series</a>
+  </li>
+
+  <li>
+    <a href="#" class="navbar-link">About</a>
+  </li>
+
+</ul>
+
+<ul class="navbar-social-list">
+
+  <li>
+    <a href="#" class="navbar-social-link">
+      <ion-icon name="logo-twitter"></ion-icon>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" class="navbar-social-link">
+      <ion-icon name="logo-facebook"></ion-icon>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" class="navbar-social-link">
+      <ion-icon name="logo-pinterest"></ion-icon>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" class="navbar-social-link">
+      <ion-icon name="logo-instagram"></ion-icon>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" class="navbar-social-link">
+      <ion-icon name="logo-youtube"></ion-icon>
+    </a>
+  </li>
+
+</ul>
+
+</nav> 
         <form id="form">
             <input type="text" autocomplete="off" id="search" placeholder="Search" class="search">
         </form>
     </header>
+   
     <main id="main">
    
     </main>
+ 
+    <section class="cta">
+        <div class="container">
+
+          <div class="title-wrapper">
+            <h2 class="cta-title">Trial start first 30 days.</h2>
+
+            <p class="cta-text">
+              Enter your email to create or restart your membership.
+            </p>
+          </div>
+
+          <form action="" class="cta-form">
+            <input type="email" name="email" required placeholder="Enter your email" class="email-field">
+
+            <button type="submit" class="cta-form-btn">Get started</button>
+          </form>
+
+        </div>
+      </section>
+
+    </article>
+  </main>
+
+
+
+
+
+  <!-- 
+    - #FOOTER
+  -->
+  <section class="service">
+        <div class="container">
+
+          <div class="service-banner">
+            <figure>
+              <img src="service-banner.jpg" alt="HD 4k resolution! only $3.99">
+            </figure>
+
+            <a href="./assets/images/service-banner.jpg" download class="service-btn">
+              <span>Download</span>
+
+              <ion-icon name="download-outline"></ion-icon>
+            </a>
+          </div>
+
+          <div class="service-content">
+
+            <p class="service-subtitle">Our Services</p>
+
+            <h2 class="h2 service-title">Download Your Shows Watch Offline.</h2>
+
+            <p class="service-text">
+              Lorem ipsum dolor sit amet, consecetur adipiscing elseddo eiusmod tempor.There are many variations of
+              passages of lorem
+              Ipsum available, but the majority have suffered alteration in some injected humour.
+            </p>
+
+            <ul class="service-list">
+
+              <li>
+                <div class="service-card">
+
+                  <div class="card-icon">
+                    <ion-icon name="tv"></ion-icon>
+                  </div>
+
+                  <div class="card-content">
+                    <h3 class="h3 card-title">Enjoy on Your TV.</h3>
+
+                    <p class="card-text">
+                      Lorem ipsum dolor sit amet, consecetur adipiscing elit, sed do eiusmod tempor.
+                    </p>
+                  </div>
+
+                </div>
+              </li>
+
+              <li>
+                <div class="service-card">
+
+                  <div class="card-icon">
+                    <ion-icon name="videocam"></ion-icon>
+                  </div>
+
+                  <div class="card-content">
+                    <h3 class="h3 card-title">Watch Everywhere.</h3>
+
+                    <p class="card-text">
+                      Lorem ipsum dolor sit amet, consecetur adipiscing elit, sed do eiusmod tempor.
+                    </p>
+                  </div>
+
+                </div>
+              </li>
+
+            </ul>
+
+          </div>
+
+        </div>
+      </section>
+
+    <footer class="footer">
+
+<div class="footer-top">
+  <div class="container">
+
+    <div class="footer-brand-wrapper">
+
+      <a href="./index.html" class="logo">
+        <img src="logo (2).png" alt="Filmlane logo">
+      </a>
+
+      <ul class="footer-list">
+
+        <li>
+          <a href="./index.html" class="footer-link">Home</a>
+        </li>
+
+        <li>
+          <a href="#" class="footer-link">Movie</a>
+        </li>
+
+        <li>
+          <a href="#" class="footer-link">TV Show</a>
+        </li>
+
+        <li>
+          <a href="#" class="footer-link">Web Series</a>
+        </li>
+
+        <li>
+          <a href="#" class="footer-link">Pricing</a>
+        </li>
+
+      </ul>
+
+    </div>
+
+    <div class="divider"></div>
+
+    <div class="quicklink-wrapper">
+
+      <ul class="quicklink-list">
+
+        <li>
+          <a href="#" class="quicklink-link">Faq</a>
+        </li>
+
+        <li>
+          <a href="#" class="quicklink-link">Help center</a>
+        </li>
+
+        <li>
+          <a href="#" class="quicklink-link">Terms of use</a>
+        </li>
+
+        <li>
+          <a href="#" class="quicklink-link">Privacy</a>
+        </li>
+
+      </ul>
+
+      <ul class="social-list">
+
+        <li>
+          <a href="#" class="social-link">
+            <ion-icon name="logo-facebook"></ion-icon>
+          </a>
+        </li>
+
+        <li>
+          <a href="#" class="social-link">
+            <ion-icon name="logo-twitter"></ion-icon>
+          </a>
+        </li>
+
+        <li>
+          <a href="#" class="social-link">
+            <ion-icon name="logo-pinterest"></ion-icon>
+          </a>
+        </li>
+
+        <li>
+          <a href="#" class="social-link">
+            <ion-icon name="logo-linkedin"></ion-icon>
+          </a>
+        </li>
+
+      </ul>
+
+    </div>
+
+  </div>
+</div>
+
+<div class="footer-bottom">
+  <div class="container">
+
+
+
+    <img src="./footer-bottom-img.png" alt="Online banking companies logo" class="footer-bottom-img">
+
+  </div>
+</div>
+
+</footer>
+
 
 <!-- js here -->
     <script src="script.js"></script>
